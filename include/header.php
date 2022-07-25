aa19b797dfb289e0bafb6b34996949efbaec3300
Enock Ndagijimana <58587259+enock-meston@users.noreply.github.com>
2022-07-25 10:40:43 +0200

    login

---
+++ include/header.php
@@ -0,0 +1,70 @@
+<!-- Header Starts -->
+<div class="navbar-wrapper ">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-fixed-top animated fadeInDown" role="navigation" id="top-nav">
+          <div class="container">
+            <div class="navbar-header">
+              <!-- Logo Starts -->
+              <a class="navbar-brand" href="#home"><img src="assets/images/logo1.png" height="30" alt="logo"/></a>
+              <!-- #Logo Ends -->
+
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+
+
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right" >
+                  <li class="active"><a href="#home">Home</a></li>
+                 <li><a href="#about">About</a></li>
+                 <li><a href="#playlist">Playlist</a></li>
+                 <li><a href="#album">Albums</a></li>
+                 <li><a href="#blogevent">Events & Blog</a></li>
+                 <li><a href="#contact">Contact</a></li>
+                 <li><a href="#"  data-toggle="modal" data-target="#loginModel">Login</a></li>
+              </ul>
+            </div>
+            <!-- #Nav Ends -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+<!-- #Header Starts -->
+
+
+
+
+<!-- login Form Model -->
+
+
+<!--Contact Starts-->
+<div class="modal fade" id="loginModel" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="contactform center">
+            <div class="row">      
+                <div class="col-sm-6 col-sm-offset-3 ">
+                <h4><b>Login Here!</b></h4>
+                    <input type="text" name="username" placeholder="UserName Or Email">
+                    <input type="password" name="password" placeholder="Password">
+                    <input type="submit" class="btn btn-warning bgcolor" name="loginbtn" value="Login">
+                </div>
+                <br>
+            </div>
+    </div>
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!--  -->
