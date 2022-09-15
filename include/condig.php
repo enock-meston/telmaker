4563b9038836016b708edc24ebace092f986901d
Enock Ndagijimana <58587259+enock-meston@users.noreply.github.com>
2022-09-15 10:28:56 +0200

    add_emial_outlook

--- include/condig.php
+++ include/condig.php
@@ -32,17 +32,17 @@ $mail = new PHPMailer(true);
 //Server settings
 //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 $mail->isSMTP();                                            //Send using SMTP
-$mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+$mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'pro@nigoote.com';                     //SMTP username
-$mail->Password   = 'Enock@123';                               //SMTP password
+$mail->Username   = 'talmaker@outlook.com';                     //SMTP username
+$mail->Password   = '0780537070@123Ta';                               //SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 //Recipients
-$mail->setFrom('pro@nigoote.com');
+$mail->setFrom('talmaker@outlook.com');
 $mail->addAddress($to);                  //Name is optional
-$mail->addReplyTo('pro@nigoote.com');
+$mail->addReplyTo('talmaker@outlook.com');
 
 
 //$body = mysqli_real_escape_string($con, $content);
